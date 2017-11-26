3ede6867041d772dee0cc3328f17df635f0a7780
泉　陽子 <yizumi@nttpc.co.jp>
2017-11-26 09:28:15 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-# config valid only for current version of Capistrano
 lock '3.6.0'
 
 set :application, 'exam3'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '13.112.13.148', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/User/yizumi.ssh/id_rsa'
+set :ssh_options, keys: '/Users/yizumi/.ssh/id_rsa'
