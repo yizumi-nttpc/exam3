3b8927c70c05fc1c0955e4e27fa2b07a472ef0d7
泉　陽子 <yizumi@nttpc.co.jp>
2017-12-02 01:36:39 +0900

    seed.rb update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-50.times do |n|
+100.times do |n|
   email = Faker::Internet.email
   name = Faker::StarTrek.character
   uid = Faker::Code.isbn
